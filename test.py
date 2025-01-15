345f6c5eec2d0c6133b217a562862ae6ae22ddec
JJ <joshua.james918@gmail.com>
2025-01-14 21:15:24 -0600

    for loops

--- test.py
+++ test.py
@@ -1,5 +1,8 @@
 # dirth dishes in the sink
 sink = [ 'bowl', 'plate','cup']
 
-for dish in sink:
+for dish in list (sink):
     print('Putting {} in the dishwasher'.format(dish))
+    sink.remove(dish)
+    
+    print(sink)
