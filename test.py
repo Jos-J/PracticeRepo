40c9c19dca8396f2ebd5d53da747ae80e5ee5680
JJ <joshua.james918@gmail.com>
2025-01-14 21:40:14 -0600

    breakstatements

---
+++ test.py
@@ -0,0 +1,15 @@
+import random
+# 20 clean dishes in the dishwasher
+dishwasher = [ 'plate', 'bowl','cup', 'knife', 'fork',
+                'spoon', 'plate', 'spoon', 'bowl', 'cup',
+                'knife', 'cup', 'cup', 'fork', 'bowl',
+                'fork', 'plate', 'cup', 'spoon', 'knife']
+
+for dish in list (dishwasher):
+    #check space left in cabinet
+    if not random.randint (0,19):
+        print('Out of space!')
+        break
+    else:
+        print('Putting {} in the Cabinet'.format(dish))
+        dishwasher.remove(dish)
