3d676052e8165fd19876db2e3a637db44774a47d
JJ <joshua.james918@gmail.com>
2025-01-15 19:22:19 -0600

    common algorithms

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
-// 20 clean dishes in the dishwasher
-let dishwasher = ['plate', 'bowl', 'cup', 'knife', 'fork',
-    'spoon', 'plate', 'spoon', 'bowl', 'cup',
-    'knife', 'cup', 'cup', 'fork', 'bowl',
-    'fork', 'plate', 'cup', 'spoon', 'knife'];
+// // 20 clean dishes in the dishwasher
+// let dishwasher = ['plate', 'bowl', 'cup', 'knife', 'fork',
+//     'spoon', 'plate', 'spoon', 'bowl', 'cup',
+//     'knife', 'cup', 'cup', 'fork', 'bowl',
+//     'fork', 'plate', 'cup', 'spoon', 'knife'];
 
-// Function to get a random integer between min (inclusive) and max (inclusive)
-function getRandomInt(min, max) {
-return Math.floor(Math.random() * (max - min + 1)) + min;
+// // Function to get a random integer between min (inclusive) and max (inclusive)
+// function getRandomInt(min, max) {
+// return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
+
+// // Process each dish in the dishwasher
+// while (dishwasher.length > 0) {
+// let dish = dishwasher[0];  // Get the first dish from the array
+
+// // Check space left in the cabinet
+// if (getRandomInt(0, 19) === 0) {
+// console.log('Out of space!');
+// break;
+// } else {
+// console.log(`Putting ${dish} in the Cabinet`);
+// dishwasher.splice(0, 1);  // Remove the first dish from the dishwasher
+// }
+// }
+
+
+function gcd(a, b) {
+    while (b !== 0) {
+        let t = a;
+        a = b;
+        b = t % b;
+    }
+    return a;
 }
 
-// Process each dish in the dishwasher
-while (dishwasher.length > 0) {
-let dish = dishwasher[0];  // Get the first dish from the array
-
-// Check space left in the cabinet
-if (getRandomInt(0, 19) === 0) {
-console.log('Out of space!');
-break;
-} else {
-console.log(`Putting ${dish} in the Cabinet`);
-dishwasher.splice(0, 1);  // Remove the first dish from the dishwasher
-}
-}
+console.log(gcd(60, 96));
+console.log(gcd(20, 8));
--- test.py
+++ test.py
@@ -13,3 +13,15 @@ for dish in list (dishwasher):
     else:
         print('Putting {} in the Cabinet'.format(dish))
         dishwasher.remove(dish)
+
+        def gcd(a, b):
+            while (b != 0):
+                t = a
+                a = b
+                b = t % b
+
+            return a
+        
+
+        print(gcd(60, 96))
+        print(gcd(20, 8))
