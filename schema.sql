498c36958d43523e1d1da2a4482326fc5c2a6139
JJ <joshua.james918@gmail.com>
2025-02-23 19:35:52 -0600

    created table family with values

--- schema.sql
+++ schema.sql
@@ -1,6 +1,13 @@
-CREATE TABLE testTable2 (
-    ID INT,
-    Name VARCHAR(40),
-    Birthday DATE
+CREATE TABLE Family (
+    ID SMALLINT UNSIGNED,
+    Name VARCHAR(60),
+    Salary DECIMAL (7,2),
+    PRIMARY KEY (ID)
 );
+
+INSERT INTO Family (ID, Name, Salary)
+VALUES
+(5, 'Jane Smith', 63000.45),
+(6, 'Bob Johnson', 50000.13),
+(7, 'Michael Brown',75000.36);
 
