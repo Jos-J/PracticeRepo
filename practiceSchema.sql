cfe298c100d2f93896b9e1cf9c83014d74b4895a
JJ <joshua.james918@gmail.com>
2025-03-20 22:10:03 -0500

    update avengers database

--- practiceSchema.sql
+++ practiceSchema.sql
@@ -1,11 +1,44 @@
 --  SHOW databases;
 --  USE dirty_jimmy;
 
---  CREATE TABLE coffee_table (
+-- CREATE TABLE end_table (
+--     id int,
+--     name VARCHAR(250),
+--     region VARCHAR(250),
+--     roast VARCHAR(250)
+-- );
+
+-- INSERT INTO end_table value (1, "default", "ethiopa", "dark");
+
+-- INSERT INTO end_table (id, name, region, roast)
+-- VALUES
+-- (2,"docker run","mexico", "dark"),
+-- (3,"help desk", "honduras", "medium"),
+-- (4, "on-call", "peru", "medium"),
+-- (5, "ifconfig", "tanzania", "coffee"),
+-- (6, "traceroute", "bali", "med-dark");
+
+
+
+--  CREATE TABLE avengers (
 --     id INT,
---     name VARCHAR(100),
---     region VARCHAR(50),
---     roast VARCHAR(25)
+--     first_name VARCHAR(30),
+--     last_name VARCHAR(30),
+--     orgin VARCHAR(25),
+--     age INT,
+--     alias VARCHAR(30)
 --  );
 
-DESCRIBE coffee_table;
+-- INSERT INTO avengers (id, first_name, last_name, orgin, age, alias)
+-- VALUES
+-- (1, "thor", "odinson", "asgard", 1500, "strongest avenger"),
+-- (2, "sam", "wilson", "earth", 35, "captain america"),
+-- (3, "miles", "morales", "earth", 16, "spiderman"),
+-- (4, "tony", "stark", "earth", 35, "iron man"),
+-- (5, "john", "stewart", "earth", 25, "green lantern");
+
+-- INSERT INTO avengers (id, first_name, last_name, orgin, age, alias)
+-- value
+-- (6, "dougie", "fresh", "a vagina", 45, "rando");
+
+SELECT * FROM avengers;
