9bd3a1fd11ff886a73dc38b356968027d35dee1f
JJ <joshua.james918@gmail.com>
2025-03-20 20:21:21 -0500

    whatever again

---
+++ practiceSchema.sql
@@ -0,0 +1,11 @@
+--  SHOW databases;
+--  USE dirty_jimmy;
+
+--  CREATE TABLE coffee_table (
+--     id INT,
+--     name VARCHAR(100),
+--     region VARCHAR(50),
+--     roast VARCHAR(25)
+--  );
+
+DESCRIBE coffee_table;
