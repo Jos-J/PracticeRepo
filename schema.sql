dcdaa6225508e1fafb2174521d9e61deafe9f066
JJ <joshua.james918@gmail.com>
2025-04-14 21:15:34 -0500

    alter query

--- schema.sql
+++ schema.sql
@@ -11,3 +11,23 @@ VALUES
 (6, 'Bob Johnson', 50000.13),
 (7, 'Michael Brown',75000.36);
 
+
+
+
+
+-- Add Producer column
+ALTER TABLE Movie
+ADD COLUMN Producer VARCHAR(50);
+
+-- Drop Genre column
+ALTER TABLE Movie
+DROP COLUMN Genre;
+
+-- Rename Year to ReleaseYear
+ALTER TABLE Movie
+RENAME COLUMN Year TO ReleaseYear;
+
+-- Change ReleaseYear data type to SMALLINT
+ALTER TABLE Movie
+MODIFY COLUMN ReleaseYear SMALLINT;
+
